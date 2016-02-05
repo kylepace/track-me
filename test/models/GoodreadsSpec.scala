6a5c44c670c722108c4b46ae8b0f5ea28f381cf0
kylepace <kylepace03@gmail.com>
2016-02-04 21:46:30 -0500

    Goodreads auth

--- test/models/GoodreadsSpec.scala
+++ test/models/GoodreadsSpec.scala
@@ -1,26 +1,17 @@
 package models
 
+import controllers.ApiAuth
 import org.junit.runner.RunWith
 import org.specs2.mutable._
 import org.specs2.runner.JUnitRunner
-import play.api.libs.oauth.{ConsumerKey, OAuth, ServiceInfo}
 
 @RunWith(classOf[JUnitRunner])
 class GoodReadsSpec extends Specification {
-  val key = "vq5wD2sQi3iVi4uWc2uffQ"
-  val secret = "W2j84y2h9gYI3cvMWxOk1DMiQcpYunk34zZcfee34"
-
-  val KEY = ConsumerKey(key, secret)
-
-  val GoodReadsOAuth = OAuth(ServiceInfo(
-    "http://www.goodreads.com/oauth/request_token",
-    "http://www.goodreads.com/oauth/access_token",
-    "http://www.goodreads.com/oauth/authorize", KEY),
-    true)
 
   "GoodReads OAuth" should {
     "return request token" in {
-      GoodReadsOAuth.retrieveRequestToken("http://localhost:9000/goodreadsaccept") match {
+      val apiAuth = new ApiAuth()
+      apiAuth.getGoodReadsRequestToken match {
         case Right(t) => t.token must not be null
         case Left(e) => throw e
       }
